f80807d350ce57dbe2a85fa6ef4d0d3ddeba928e
Marcin Dubiel <fortfel@gmail.com>
2026-01-04 21:32:11 +0100

    update: removed some unused UI components

--- packages/ui/src/components/vaul/types.ts
+++
@@ -1,9 +0,0 @@
-// copied from https://github.com/emilkowalski/vaul
-// @ts-nocheck
-export type DrawerDirection = 'top' | 'bottom' | 'left' | 'right';
-export interface SnapPoint {
-  fraction: number;
-  height: number;
-}
-
-export type AnyFunction = (...args: any) => any;
